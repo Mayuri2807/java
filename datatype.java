59aa7e53f5e1d43dffca4194f31079e462142f9c
mayuri mokate <165499858+Mayuri2807@users.noreply.github.com>
2024-08-01 23:46:59 +0530

    Add files via upload

---
+++ datatype.java
@@ -0,0 +1,16 @@
+class datatype
+{
+	public static void main(String arg[])
+	{
+     int no=11;
+     float f=90.99f;
+     char ch='a';
+
+     System.out.println(no);
+     System.out.println(f);
+
+    System.out.println(ch);
+
+
+	}
+}
